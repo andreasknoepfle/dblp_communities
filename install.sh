e06f4ca764e761eb012fb71a83f1ec741682246b
andreasknoepfle <andreas.knoepfle@googlemail.com>
2011-12-04 18:05:57 +0100

    Added DBLP install script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+
+# Vars
 filename="Community_BGLL_CPP"
 zip="$filename.zip"
 url="https://sites.google.com/site/findcommunities/"
+dblp_server="http://dblp.uni-trier.de/xml/"
+dblp_xml="dblp.xml"
+dblp_dtd="dblp.dtd"
+dblp_dir="./dblp"
+#Skript
+
 echo "Getting Remote Resources"
 if [ ! -f $zip ]; then
 echo "Downloading Community Detection Algorithm from $url"
@@ -15,4 +23,17 @@ else
 fi
 cd $filename
 make
- 
+echo "Downloading DBLP"
+cd ..
+echo "Creating DBLP Directory"
+mkdir -p $dblp_dir
+
+cd dblp
+if [ ! -f $dblp_xml ]; then
+wget $dblp_server$dblp_xml
+fi
+if [ ! -f $dblp_dtd ]; then
+wget $dblp_server$dblp_dtd
+fi
+cd ..
+
